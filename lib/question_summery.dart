9d113b72f3ec848e7f6792a155f22129b3bd66cc
pyaesoneaung <pyaesone@blue-stone.net>
2024-03-27 18:53:13 +0630

    87 - Making Content Scrollable with SingleChildScrollView

--- lib/question_summery.dart
+++ lib/question_summery.dart
@@ -2,41 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class QuestionSummery extends StatelessWidget {
+  const QuestionSummery(this.summeryData, {super.key});
 
-  const QuestionSummery(this.summeryData,{super.key});
-
-  final List<Map<String,Object>> summeryData;
+  final List<Map<String, Object>> summeryData;
 
   @override
   Widget build(context) {
-    return  Column(
-      children : summeryData.map(
-        (data) {
-          return Row(
-            children: [
-              Text(
-                (((data['question_index']as int) + 1).toString()),
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text(
-                      data['question'] as String
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summeryData.map(
+            (data) {
+              return Row(
+                children: [
+                  Text(
+                    (((data['question_index'] as int) + 1).toString()),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(data['question'] as String),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text((data['user_answer'] as String)),
+                        Text((data['correct_answer'] as String))
+                      ],
                     ),
-                    const SizedBox(height: 5,),
-                    Text(
-                      (data['user_answer'] as String)
-                    ),
-                    Text(
-                      (data['correct_answer'] as String)
-                    )
-                  ],
-                ),
-              )
-            ],
-          );
-        },
-      ).toList(),
+                  )
+                ],
+              );
+            },
+          ).toList(),
+        ),
+      ),
     );
   }
 }
