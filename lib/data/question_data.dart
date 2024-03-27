22b5c007c116d67dc6ac94d171631a5d382fbd81
pyaesoneaung <pyaesone@blue-stone.net>
2024-03-27 12:02:47 +0630

    66 - Adding a Data Model Dummy Data

---
+++ lib/data/question_data.dart
@@ -0,0 +1,40 @@
+import 'package:quiz_app/models/question_model.dart';
+
+const questionData = [
+  QuestionModel(
+    'How is pyae sone aung\'s relationship?',
+    [
+      'Single',
+      'Complicated',
+      'Marriage',
+      'In Relationship'
+    ]
+  ),
+  QuestionModel(
+    'What is pyae sone aung\'s Job?',
+    [
+      'Full Stack Developer',
+      'Java Developer',
+      'Cupid',
+      'Ma Ma Maung Lay'
+    ]
+  ),
+  QuestionModel(
+    'Where is pyae sone live?',
+    [
+      'In Ma Ma Heart',
+      'Htauk Kyant',
+      'Maubin',
+      'Sanchaung'
+    ]
+  ),
+  QuestionModel(
+    'Who is pyae sone\s girlfriend?',
+    [
+      'Chae Soo-bin',
+      'Han Hyo-Jo',
+      'Go Youn-jung',
+      'Park Min Young'
+    ]
+  ),
+];
---
+++ lib/models/question_model.dart
@@ -0,0 +1,7 @@
+class QuestionModel {
+
+  const QuestionModel(this.title,this.possibleAnswers);
+
+  final String title;
+  final List<String> possibleAnswers;
+}
