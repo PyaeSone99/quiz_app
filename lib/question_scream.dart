74fab0bcaafdf5999326ae4a4ae8c40570024c56
pyaesoneaung <pyaesone@blue-stone.net>
2024-03-27 15:30:35 +0630

    69 - Accessing List Elements Object Properties

--- lib/question_scream.dart
+++ lib/question_scream.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/question_data.dart';
 
 class QuestionScream extends StatefulWidget {
 
@@ -13,6 +14,8 @@ class QuestionScream extends StatefulWidget {
 
 
 class _QuestionScreamState extends State<QuestionScream> {
+
+  var currentQuestion = questionData[0];
   
   @override
   Widget build(context) {
@@ -21,11 +24,14 @@ class _QuestionScreamState extends State<QuestionScream> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-           const Text('This is title'),
+          Text(currentQuestion.title,style: const TextStyle(
+            color: Colors.white
+          ),),
            const SizedBox(height: 20,),
-           AnswerButton(onTab: (){}, answerText: 'Answer 1'),
-           AnswerButton(onTab: (){}, answerText: 'Answer 1'), 
-           AnswerButton(onTab: (){}, answerText: 'Answer 1'),       
+           AnswerButton(onTab: (){}, answerText: currentQuestion.possibleAnswers[0]),
+           AnswerButton(onTab: (){}, answerText: currentQuestion.possibleAnswers[1]), 
+           AnswerButton(onTab: (){}, answerText: currentQuestion.possibleAnswers[2]),
+           AnswerButton(onTab: (){}, answerText: currentQuestion.possibleAnswers[3]),      
           ],
       ),
     );
