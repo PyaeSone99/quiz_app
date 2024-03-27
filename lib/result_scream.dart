84ee33026ee722c1f7b3010a8adaa05188fb0ac9
pyaesoneaung <pyaesone@blue-stone.net>
2024-03-27 17:56:01 +0630

    80 - Introducing Maps for Loops

--- lib/result_scream.dart
+++ lib/result_scream.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/question_data.dart';
 
 class ResultScream extends StatelessWidget {
 
   const ResultScream({super.key,required this.chosenAnswered});
 
   final List<String> chosenAnswered;
+
+  List<Map<String,Object>> getSummeryData (){
+    List<Map<String,Object>> summery = [];
+
+    for (var i = 0; i < chosenAnswered.length; i++) {
+      summery.add({
+        'question_indes' : i,
+        'question' : questionData[i],
+        'correct_answer' : questionData[i].possibleAnswers[0],
+        'user_answer' : chosenAnswered[i]
+      });
+    }
+
+    return summery;
+  }
 
   @override
   Widget build(context) {
